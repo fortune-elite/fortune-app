6e2ee351b36c98997c59748b2633c658d5e9f69d
merdikimuha1@gmail.com <merdikimuha1@gmail.com>
2021-06-15 08:18:48 -0700

    set navbar

--- fortune-app/src/App.js
+++ fortune-app/src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App__name">Fortune App</h1>
+      <Navbar/>
     </div>
   );
 }
