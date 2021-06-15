044998784e38ea236d97ffaf4fe677f676f56af5
merdikimuha1@gmail.com <merdikimuha1@gmail.com>
2021-06-15 15:05:22 -0700

    finish the introduction part

--- fortune-app/src/components/Introduction.js
+++ fortune-app/src/components/Introduction.js
@@ -9,16 +9,33 @@ const Introduction = () => {
         <div className="introduction__upper">
             <h1 className= "introduction__title">Investing and Saving <br></br>in a better way</h1>
             <section>
-                Fortune helps customers to achieve their monetary goals by helping them Save & invest, Fissure bills, Obtaining a loan. Control your money with spending<br></br> analytics
+                Fortune helps customers to achieve their<br></br> monetary goals by helping them Save & <br></br>invest, Fissure bills, Obtaining a loan. <br></br>Control your money with spending<br></br> analytics
             </section>
             <button className="btn__create-account">Create A Free Account</button>
             <section className="download-app">
-                <div className="ios">App Store</div>
-                <div className="android">Play Store</div>
+                <div className="ios">
+                    <section>
+                        <img src="logo192.png" width={50}></img>
+                    </section>
+                    <section>
+                        Download from <br></br>
+                        <span>App Store</span>
+                    </section>
+                </div>
+                <div className="android">
+                    <section>
+                        <img src="logo192.png" width={50}></img>
+                    </section>
+                    <section>
+                        <span>Get it on </span>
+                        <span>Play Store</span>
+                    </section>
+                </div>
             </section>
         </div>
         <div className="introduction__lower">
-            
+            <p className="lower-one">Fortune, the solution towards a better financial future </p>
+            <p className="lower-two">We help you save money without putting forth a lot of effort with <br></br> computerized savings </p>
         </div>
 
      </div>
