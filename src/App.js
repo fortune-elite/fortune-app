020d593928b9a69f879df84c0eb09ef18f89217e
oluwatunbi <davies899@yahoo.com>
2021-06-25 21:33:43 +0100

    my new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
+import Landingpage from './pages/landingpage/landingpage.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App__name">Fortune App</h1>
+    <Landingpage/>
     </div>
   );
 }
---
+++ src/pages/landingpage/landingpage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Services from '../../components/services/sevices.js';
+
+const Landingpage = () => {
+    return(
+        <div>  
+            <Services/>
+           { /* <Navigation/>
+            <Introduction/>
+          
+            <Savepad/>
+            <Howto/> */ }
+        </div>
+    )
+}
+
+
+export default Landingpage;
