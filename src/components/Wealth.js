6902588b2fdd5546ee9e9a483fdd74bb251bdd38
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-07-08 12:13:35 +0100

    Completed the ipad responsiveness on wealth.js

--- src/components/Wealth.js
+++ src/components/Wealth.js
@@ -3,8 +3,44 @@ import "./css/Wealth.modules.css"
 const Wealth = () => {
     return ( 
         <div className="Wealth">
-            <h1>Hello</h1>
+            <div>
+                <h1>Start building your wealth in 3 minutes</h1>
+            </div>
 
+            <div>
+            <h2>
+             <i className="fas fa-square"></i> Create an Accout with Us
+             </h2>
+  
+                <p>Create an account with us today with just your name, email and phone Number.</p>
+            </div>
+
+            <div>
+            <h2>
+             <i className="fas fa-square"></i> Set up your security
+             </h2>
+                <p>Input your password/pin, fingerprint and Two-factor Authentication (2FA) to securely protect your account from unauthorized access/impersonation.</p>
+            </div>
+
+            <div>
+            <h2>
+             <i className="fas fa-square"></i> Add a payment Method
+            </h2>
+
+                <p>Using your debit card, bank account, USSD, QR code, setup your first plan</p>
+            </div>
+
+            <div>
+            <h2>
+             <i className="fas fa-square"></i> Monitor Your Earnings
+             </h2>
+                <p>Sit back, relax & watch as your money works for you all day, everyday</p>
+            </div>
+
+            <div>
+                <button>Sign Up Now</button>
+
+            </div>
         </div>
      );
 }
