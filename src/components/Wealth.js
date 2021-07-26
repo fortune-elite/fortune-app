a66c157e7a5c05b77ed45d48e4dd88dd321cf476
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-07-26 15:55:53 +0100

    Working

--- src/components/Wealth.js
+++ src/components/Wealth.js
@@ -3,13 +3,13 @@ import "./css/Wealth.modules.css"
 const Wealth = () => {
     return ( 
         <div className="Wealth">
-            <div>
+            <div className="mt-4">
                 <h1>Start building your wealth in 3 minutes</h1>
             </div>
 
             <div>
             <h2>
-             <i className="fas fa-square"></i> Create an Accout with Us
+             <i className="fas fa-square mt-4"></i> Create an Accout with Us
              </h2>
   
                 <p>Create an account with us today with just your name, email and phone Number.</p>
@@ -17,14 +17,14 @@ const Wealth = () => {
 
             <div>
             <h2>
-             <i className="fas fa-square"></i> Set up your security
+             <i className="fas fa-square mt-4"></i> Set up your security
              </h2>
                 <p>Input your password/pin, fingerprint and Two-factor Authentication (2FA) to securely protect your account from unauthorized access/impersonation.</p>
             </div>
 
             <div>
             <h2>
-             <i className="fas fa-square"></i> Add a payment Method
+             <i className="fas fa-square mt-4"></i> Add a payment Method
             </h2>
 
                 <p>Using your debit card, bank account, USSD, QR code, setup your first plan</p>
@@ -32,12 +32,12 @@ const Wealth = () => {
 
             <div>
             <h2>
-             <i className="fas fa-square"></i> Monitor Your Earnings
+             <i className="fas fa-square mt-4"></i> Monitor Your Earnings
              </h2>
                 <p>Sit back, relax & watch as your money works for you all day, everyday</p>
             </div>
 
-            <div>
+            <div className="mt-4">
                 <button>Sign Up Now</button>
 
             </div>
