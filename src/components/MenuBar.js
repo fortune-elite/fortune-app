410833fca4c7fce021fbd4b32a54b2f4d99648c3
Quincy Oghenetejiri <ukumakubequincy@gmail.com>
2021-07-27 21:50:37 +0100

    Font-Size Changes

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './css/MenuBar.css';
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai';
+import {AiOutlineMenu} from 'react-icons/ai';
+import {FaTimes} from 'react-icons/fa';
 import { useState } from 'react';
 
 
@@ -18,7 +19,7 @@ const MenuBar = () => {
       <div>
           <AiOutlineMenu className='right-menu' onClick = {showMenuBar}/>
           <div className={`MenuBar ${menuBar && 'active'}`} onClick={showMenuBar}>
-              <button className="but"><AiOutlineClose className='closeButton'/></button>
+              <button className="but"><FaTimes className='closeButton'/></button>
               <ul className ='drop'>
                 <li>Save</li>
                 <li>Invest</li>
